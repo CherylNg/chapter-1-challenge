ce40ad34e29b38184bb595cf9366331b2c1fdf0f
CherylNg <61680146+CherylNg@users.noreply.github.com>
2020-05-11 11:13:15 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Silly)
+basic.showIcon(IconNames.Surprised)
+basic.showIcon(IconNames.Fabulous)
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+basic.showString("EDU:BIT CLASS")
+basic.forever(function () {
+    basic.showString("16 MAY 2020 :")
+    basic.showString("NO HOMEWORK")
+})
